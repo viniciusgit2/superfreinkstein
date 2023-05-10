91bff1bcbf83fc2e689da5cbb9b982e50ed716f4
viniciusgit2 <4xv6patelfin@gmail.com>
2023-05-10 09:18:47 -0300

    começo dos começo

---
+++ src/app/music/spote/main.js
@@ -0,0 +1,4 @@
+export const search = (query, type) =>
+  fetch(`https://api.spotify.com/${query}&${type}`).then((data) => data.json());
+export const playlist = (query, type) =>
+  fetch(`https://api.spotify.com/${query}&${type}`).then((data) => data.json());
